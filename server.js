c4678968ed64a54c70a0ee47bd9037568600f6bb
Snehith0812 <nsnehithreddy@gmail.com>
2026-03-29 21:21:28 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -107,6 +107,6 @@ app.get("/api/export", auth, async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
